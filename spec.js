defab7f72ba92669a30389d9589d4ef83a1a61a4
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2014-08-28 11:54:54 +0900

    ステップ2を終わらせた

--- spec.js
+++ spec.js
@@ -1,13 +1,33 @@
 describe('AngularJSホームページ', function() {
-  it('1と2を加える', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+
+  beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+  });
 
-    element(by.id('gobutton')).click();
+  it('タイトルを持つ', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
 
-    expect(element(by.binding('latest')).getText()).
-        //toEqual('5'); // これはまちがい！
-        toEqual('3');
+  it('1と2を加える', function() {
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('4と6を加える', function() {
+    // この部分を自分で書いて下さい
+    firstNumber.sendKeys(4);
+    secondNumber.sendKeys(6);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('10');
   });
 });
