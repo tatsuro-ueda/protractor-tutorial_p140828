77e41c6234ee4d9e966c67f5148a70a148e818db
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2014-08-28 11:40:35 +0900

    ステップ1を終わらせた

--- spec.js
+++ spec.js
@@ -1,7 +1,13 @@
 describe('AngularJSホームページ', function() {
-  it('タイトルを持つ', function() {
+  it('1と2を加える', function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
+    element(by.model('first')).sendKeys(1);
+    element(by.model('second')).sendKeys(2);
 
-    expect(browser.getTitle()).toEqual('Super Calculator');
+    element(by.id('gobutton')).click();
+
+    expect(element(by.binding('latest')).getText()).
+        //toEqual('5'); // これはまちがい！
+        toEqual('3');
   });
 });
